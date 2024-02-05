14ba1dcba37e7fa80c7d15869be30f6d232d6008
Adoni5 <roryjmunro1@gmail.com>
2024-02-05 11:42:29 +0000

    Add average quality function for FASTQ Add Wrapper and emmiter type enums

--- src/_splitting.rs
+++ src/_splitting.rs
@@ -1,18 +1,34 @@
 use clap::ValueEnum;
 use fnv::FnvHashSet;
+use noodles::bam;
+use noodles_bgzf as bgzf;
+
 use std::{
     fs::File,
     io,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
 };
 
-#[derive(Debug, ValueEnum, Clone, Default)]
+#[derive(Debug, ValueEnum, Clone, Default, PartialEq)]
 pub enum SplitType {
-    #[default]
     All,
     UnblockedOnly,
+    #[default]
     SequencedOnly,
+}
+
+#[derive(Debug, ValueEnum, Clone, Default, PartialEq)]
+pub enum EmitType {
+    #[default]
+    Bam,
+    Fastq,
+    Fasta,
+}
+
+pub enum Wrapper {
+    Bam(bam::io::Writer<bgzf::Writer<File>>),
+    Fastx(BufWriter<File>),
 }
 
 /// Read unblocked_read_ids.txt into a HashSet
@@ -29,4 +45,16 @@ pub fn read_unblocked_read_ids(path: PathBuf) -> Result<FnvHashSet<String>, io::
         }
         Err(err) => Err(err),
     }
+}
+
+/// This function calculates the average quality of a read, and does this correctly
+/// First the Phred scores are converted to probabilities (10^(q)/-10) and summed
+/// and then divided by the number of bases/scores and converted to Phred again -10*log10(average)
+/// Taken from https://github.com/wdecoster/chopper/blob/b94a60ff075f3b69aea4ef9cb976c8064ceb5993/src/main.rs#L176
+pub fn _ave_qual(quals: &[u8]) -> f64 {
+    let probability_sum = quals
+        .iter()
+        .map(|q| 10_f64.powf(((*q) as f64) / -10.0))
+        .sum::<f64>();
+    (probability_sum / quals.len() as f64).log10() * -10.0
 }
